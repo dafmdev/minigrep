d6e30b1a13c821d29ca95d26abe83c6710b04442
Daniel Franco <daniel.franco@mercadolibre.com.co>
2023-07-14 19:00:45 -0500

    Read two arguments from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    println!("The query: {}", query);
+    println!("The file_path: {}", file_path);
 }
