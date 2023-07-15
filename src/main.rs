ebd4dce7ac1ae488968a934e1951cda810ebbf11
Daniel Franco <daniel.franco@mercadolibre.com.co>
2023-07-14 19:13:56 -0500

    read file content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +6,8 @@ fn main() {
     let query: &String = &args[1];
     let file_path: &String = &args[2];
 
+    let contents: String = fs::read_to_string(file_path).expect("Check if the filepath is correct");
+
     println!("The query: {}", query);
-    println!("The file_path: {}", file_path);
+    println!("The text:\n {}", contents);
 }
